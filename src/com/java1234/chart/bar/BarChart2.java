ad6486f20e8c90ea41142859cd6485c6823915ba
yzhihao <1079345916@qq.com>
2016-08-09 10:35:08 +0800

    JfreeChart画图类包的demo

---
+++ src/com/java1234/chart/bar/BarChart2.java
@@ -0,0 +1,24 @@
+package com.java1234.chart.bar;
+
+import javax.servlet.http.HttpSession;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.servlet.ServletUtilities;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class BarChart2 {
+
+	public static String genBarChart(HttpSession session) throws Exception {
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		dataset.addValue(510, "深圳", "苹果");
+		dataset.addValue(320, "深圳", "香蕉");
+		dataset.addValue(580, "深圳", "橘子");
+		dataset.addValue(390, "深圳", "梨子");
+		JFreeChart chart=ChartFactory.createBarChart3D("水果销售统计图", "水果", "销售", dataset,
+				PlotOrientation.HORIZONTAL, true, true, true);
+		String fileName=ServletUtilities.saveChartAsPNG(chart, 700, 500, null,session);
+		return fileName;
+	}
+}
---
+++ src/com/java1234/chart/bar/BarChart3.java
@@ -0,0 +1,24 @@
+package com.java1234.chart.bar;
+
+import javax.servlet.http.HttpSession;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.servlet.ServletUtilities;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetUtilities;
+
+public class BarChart3 {
+
+	public static String genBarChart(HttpSession session) throws Exception {
+		double [][]data=new double[][]{{1320},{720},{830},{400}};
+		String []rowKeys={"苹果","香蕉","橘子","梨子"};
+		String []columnKeys={"深圳"};
+		CategoryDataset dataset=DatasetUtilities.createCategoryDataset(rowKeys,columnKeys ,data);
+		JFreeChart chart=ChartFactory.createBarChart3D("水果销售统计图", "水果", "销售", dataset,
+				PlotOrientation.VERTICAL, true, true, true);
+		String fileName=ServletUtilities.saveChartAsPNG(chart, 700, 500, null,session);
+		return fileName;
+	}
+}
